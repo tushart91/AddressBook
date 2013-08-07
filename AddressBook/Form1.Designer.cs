87550a5e28cf2e14f40232783d14a17be46c85c1
Tushar Tiwari <tushart91@gmail.com>
2013-08-07 10:25:45 +0530

    Init Commit

---
+++ AddressBook/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace AddressBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.add = new System.Windows.Forms.Button();
+            this.remove = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(181, 292);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "People";
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(7, 20);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(166, 251);
+            this.listBox.TabIndex = 0;
+            this.listBox.SelectedIndexChanged += new System.EventHandler(this.listBox_SelectedIndexChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.add);
+            this.groupBox2.Controls.Add(this.remove);
+            this.groupBox2.Controls.Add(this.save);
+            this.groupBox2.Controls.Add(this.textBox5);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.dateTimePicker1);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(199, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(355, 292);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Information";
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(15, 247);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(105, 35);
+            this.add.TabIndex = 14;
+            this.add.Text = " Add";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // remove
+            // 
+            this.remove.Location = new System.Drawing.Point(230, 247);
+            this.remove.Name = "remove";
+            this.remove.Size = new System.Drawing.Size(105, 35);
+            this.remove.TabIndex = 13;
+            this.remove.Text = "Remove";
+            this.remove.UseVisualStyleBackColor = true;
+            this.remove.Click += new System.EventHandler(this.remove_Click);
+            // 
+            // save
+            // 
+            this.save.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.save.Location = new System.Drawing.Point(123, 247);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(104, 35);
+            this.save.TabIndex = 12;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(15, 179);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(320, 62);
+            this.textBox5.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 155);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Additional Notes";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(95, 126);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(240, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Phone No:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(95, 100);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(240, 20);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Birthday:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(95, 74);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(240, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Facebook";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(95, 48);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(240, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Email:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(95, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Full Name:";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.save;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 316);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AddressBook";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button remove;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.ListBox listBox;
+    }
+}
+
---
+++ AddressBook/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+namespace AddressBook
+{
+    public partial class Form1 : Form
+    {
+        int index;
+        List<Person> people = new List<Person>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            if (!Directory.Exists(path + "\\AddressBook"))
+                Directory.CreateDirectory(path + "\\AddressBook");
+            if (!File.Exists(path + "\\AddressBook\\settings.xml"))
+            {
+                XmlTextWriter xw = new XmlTextWriter(path + "\\AddressBook\\settings.xml", Encoding.UTF8);
+                xw.WriteStartElement("People");
+                xw.WriteEndElement();
+                xw.Close();
+            }
+            XmlDocument xdoc = new XmlDocument();
+            xdoc.Load(path + "\\AddressBook\\settings.xml");
+            foreach (XmlNode xNode in xdoc.SelectNodes("People/Person"))
+            {
+                Person p = new Person();
+                p.name = xNode.SelectSingleNode("Name").InnerText;
+                p.emailID = xNode.SelectSingleNode("Email").InnerText;
+                p.faceBook = xNode.SelectSingleNode("Facebook").InnerText;
+                p.phoneNo = xNode.SelectSingleNode("Phone").InnerText;
+                p.additionalNotes = xNode.SelectSingleNode("Notes").InnerText;
+                p.birthday = DateTime.FromFileTime( Convert.ToInt64(xNode.SelectSingleNode("Birthday").InnerText));
+                people.Add(p);
+                listBox.Items.Add(p.name);
+            }
+        }
+
+        private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                index = listBox.SelectedIndex;
+                textBox1.Text = people[index].name;
+                textBox2.Text = people[index].emailID;
+                textBox3.Text = people[index].faceBook;
+                textBox4.Text = people[index].phoneNo;
+                textBox5.Text = people[index].additionalNotes;
+                dateTimePicker1.Value = people[index].birthday;
+            }
+            catch { }
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            Person p = new Person();
+            p.name = textBox1.Text;
+            p.emailID = textBox2.Text;
+            p.faceBook = textBox3.Text;
+            p.birthday = dateTimePicker1.Value;
+            p.phoneNo = textBox4.Text;
+            p.additionalNotes = textBox5.Text;
+            people.Add(p);
+            listBox.Items.Add(p.name);
+            textBox1.Text = textBox2.Text = textBox3.Text = textBox4.Text = textBox5.Text = "";
+            dateTimePicker1.Value = DateTime.Now;
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                people[index].name = textBox1.Text;
+                people[index].emailID = textBox2.Text;
+                people[index].faceBook = textBox3.Text;
+                people[index].phoneNo = textBox4.Text;
+                people[index].additionalNotes = textBox5.Text;
+                people[index].birthday = dateTimePicker1.Value; ;
+                listBox.Items[index] = textBox1.Text;
+            }
+            catch
+            { }
+        }
+
+        private void remove_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox1.Text = textBox2.Text = textBox3.Text = textBox4.Text = textBox5.Text = "";
+                dateTimePicker1.Value = DateTime.Now;
+                people.RemoveAt(index);
+                listBox.Items.RemoveAt(index);
+            }
+            catch { }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            XmlDocument xdoc = new XmlDocument();
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            xdoc.Load(path + "\\AddressBook\\settings.xml");
+            XmlNode xnode = xdoc.SelectSingleNode("People");
+            xnode.RemoveAll();
+            foreach (Person p in people)
+            {
+                XmlNode xTop = xdoc.CreateElement("Person");
+                XmlNode xName = xdoc.CreateElement("Name");
+                XmlNode xEmail = xdoc.CreateElement("Email");
+                XmlNode xFacebook = xdoc.CreateElement("Facebook");
+                XmlNode xNotes = xdoc.CreateElement("Notes");
+                XmlNode xBirthday = xdoc.CreateElement("Birthday");
+                XmlNode xPhone = xdoc.CreateElement("Phone");
+                xName.InnerText = p.name;
+                xEmail.InnerText = p.emailID;
+                xFacebook.InnerText = p.faceBook;
+                xNotes.InnerText = p.additionalNotes;
+                xBirthday.InnerText = p.birthday.ToFileTime().ToString();
+                xPhone.InnerText = p.phoneNo;
+                xTop.AppendChild(xName);
+                xTop.AppendChild(xEmail);
+                xTop.AppendChild(xFacebook);
+                xTop.AppendChild(xNotes);
+                xTop.AppendChild(xBirthday);
+                xTop.AppendChild(xPhone);
+                xdoc.DocumentElement.AppendChild(xTop);
+            }
+            xdoc.Save(path + "\\AddressBook\\settings.xml");
+        }
+    }
+    class Person
+    {
+        public string name
+        {
+            get;
+            set;
+        }
+        public string emailID
+        {
+            get;
+            set;
+        }
+        public string faceBook
+        {
+            get;
+            set;
+        }
+        public string phoneNo
+        {
+            get;
+            set;
+        }
+        public string additionalNotes
+        {
+            get;
+            set;
+        }
+        public DateTime birthday
+        {
+            get;
+            set;
+        }
+    }
+}
